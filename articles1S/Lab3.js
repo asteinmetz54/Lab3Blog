332b08dae7ca5b22c596644cbebb188f53aeb0aa
Andrew Steinmetz <asteinmetz54@gmail.com>
2016-11-02 12:05:15 -0700

    started to add response to login submit

--- articles1S/Lab3.js
+++ articles1S/Lab3.js
@@ -3,23 +3,30 @@ var http = require('http');
 var url = require('url');
 var q = require('querystring');
 
-///Creates a server listening on port 3000. If the user types http://localhost:3000/?msg=0 
-/// or msg=1, or msg=2 it will display the files. If msg=  nothing then it will display No msg parameter. 
-/// Otherwise an error.
+///Creates a server listening on port 3000. Loads Landing page with login hyperlink
 http.createServer(function (req, res) {
     var resBody = '';
     var resMsg = '';
     var urlObj = url.parse(req.url, true, false);
     var qstr = urlObj.query;
-    if (!qstr.msg) {
+    
+    //when user submits login infomation console prints what was typed in
+    if (req.method == "POST"){
+        var reqData = '';
+        req.on('data', function(chunk){
+            reqData += chunk;
+        });
+        req.on('end', function(){
+        console.log(reqData);
+        })
+    }else if (!qstr.msg) {
         resBody = resBody + '<html><head><title>Index</title></head>\n';
         resMsg = '<h2>Blog Home Page</h2>\n';
         resMsg += '<a href="?msg=login">Login</a>';
         resBody += '<body>' + resMsg;
         resMsg = resBody + '\n</body></html>';
-        console.log(resMsg);
-    } else if(qstr.msg='login'){
-        resMsg = fs.readFileSync("blogs/login.html");
+    }else if(qstr.msg='login'){
+        resMsg = fs.readFileSync("auth/login.html");
     }else {
         resMsg = loadArt(fs.readFileSync(messages[qstr.msg]));
     }
