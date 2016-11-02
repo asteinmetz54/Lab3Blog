c2688f7a23501cc8025698022253978b1662f57a
Andrew Steinmetz <asteinmetz54@gmail.com>
2016-11-02 11:16:37 -0700

    login page loads by clicking on login from home page

--- articles1S/Lab3.js
+++ articles1S/Lab3.js
@@ -1,9 +1,7 @@
 var fs = require('fs');//Loads File System
 var http = require('http');
 var url = require('url');
-
-///These are the .art filenames to test in an array
-var messages = ["foodPoisoning.art", "weightLifting.art", "nutrition.art"];
+var q = require('querystring');
 
 ///Creates a server listening on port 3000. If the user types http://localhost:3000/?msg=0 
 /// or msg=1, or msg=2 it will display the files. If msg=  nothing then it will display No msg parameter. 
@@ -13,14 +11,16 @@ http.createServer(function (req, res) {
     var resMsg = '';
     var urlObj = url.parse(req.url, true, false);
     var qstr = urlObj.query;
-    console.log(urlObj);
     if (!qstr.msg) {
-        resBody = resBody + '<html><head><title>Simple HTTP Server</title></head>';
-        resMsg = '<h2>No msg parameter</h2>\n';
-        resBody = resBody + '<body>' + resMsg;
+        resBody = resBody + '<html><head><title>Index</title></head>\n';
+        resMsg = '<h2>Blog Home Page</h2>\n';
+        resMsg += '<a href="?msg=login">Login</a>';
+        resBody += '<body>' + resMsg;
         resMsg = resBody + '\n</body></html>';
         console.log(resMsg);
-    } else {
+    } else if(qstr.msg='login'){
+        resMsg = fs.readFileSync("blogs/login.html");
+    }else {
         resMsg = loadArt(fs.readFileSync(messages[qstr.msg]));
     }
     res.setHeader("Content-Type", "text/html");
